15b5f7d1e67da89d46a83c49215b06c0bf82d182
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-03-03 16:04:43 +0530

    Main page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import { useOrganization } from "@clerk/nextjs";
+import EmptyOrg from "./_components/empty-org";
 
 
 export default function DashboardPage() {
+  const { organization } = useOrganization()
   return (
-    <div className=" flex-1 h-[calc(100%-80px)]">DashboardPage</div>
+    <div className=" flex-1 h-[calc(100%-80px)]">
+      {
+        !organization ?
+          (<EmptyOrg />) :
+          (<p>Board List</p>)
+      }
+    </div>
   );
 }
