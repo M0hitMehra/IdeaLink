72b6c0023731503356a2f2c6de4e0c16f7cf6033
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-03-05 20:59:23 +0530

    APi setup and creation

--- app/(dashboard)/_components/sidebar/empty-board.tsx
+++ app/(dashboard)/_components/sidebar/empty-board.tsx
@@ -1,8 +1,22 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import { useOrganization } from '@clerk/nextjs'
+import { useMutation } from 'convex/react'
 import Image from 'next/image'
 import React from 'react'
 
 const EmptyBoard = () => {
+    const create = useMutation(api.board.create)
+    const { organization } = useOrganization()
+    const onClick = () => {
+        if (!organization) return
+        create({
+            orgId: organization.id,
+            tite: "Untitled",
+        })
+    }
     return (
         <div className=' h-full flex justify-center items-center flex-col'>
             <Image src={"/note.svg"} alt='Empty Boards' height={200} width={200} className=' drop-shadow-2xl' />
@@ -11,7 +25,7 @@ const EmptyBoard = () => {
             </h2>
             <p className=' text-muted-foreground text-sm mt-2'>Start creating board for your organization</p>
             <div className='mt-6'>
-                <Button size={"lg"}>
+                <Button size={"lg"} onClick={onClick}>
                     Create Boards
                 </Button>
             </div>
