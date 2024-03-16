f01750677e926237b6e04ea898bf1251dca2a5d4
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-03-16 21:54:16 +0530

    Added skeleton loading and new board button

--- convex/board.ts
+++ convex/board.ts
@@ -74,3 +74,47 @@ export const update = mutation({
         return board
     }
 })
+
+
+export const favourite = mutation({
+    args: {
+        orgId: v.string(),
+        id: v.id("boards"),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Unauthorized")
+        }
+
+        const board = await ctx.db.get(args.id)
+
+        if (!board) {
+            throw new Error("Board not found")
+        }
+
+        const userId = identity.subject
+
+        const existingFavourite = await ctx.db.query("userFavorites")
+            .withIndex("by_user_board_org",
+                (q) => q
+                    .eq("userId", userId)
+                    .eq("boardId", board._id)
+                    .eq("orgId", args.orgId)
+
+            )
+
+        if (existingFavourite) {
+            throw new Error("Favourite already exists")
+        }
+
+        const favourites = await ctx.db.insert("userFavorites", {
+            orgId: args.orgId,
+            userId,
+            boardId: board._id
+        })
+
+        return board;
+    }
+
+})
