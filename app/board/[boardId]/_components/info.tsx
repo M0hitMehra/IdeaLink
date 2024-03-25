b5479bc9908822fc88a931dc6fd1095fee5a813a
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-03-25 17:49:17 +0530

    Added boards

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -14,6 +14,7 @@ const Info = ({ boardId }: InfoProps) => {
 
 export default Info
 
+
 export const InfoSkeleton = () => {
     return <div className=' absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]' />
 }
