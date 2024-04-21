6fcfbea27c8ff91e0adec6e539692865b22e02d7
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-04-21 19:18:04 +0530

    Second commit

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useSelf } from "@/liveblocks.config";
-import { Side, XYWH } from "@/types/Canvas";
+import { useSelf, useStorage } from "@/liveblocks.config";
+import { LayerType, Side, XYWH } from "@/types/Canvas";
 import { memo } from "react";
 
 interface SelectionBoxProps {
@@ -12,6 +12,8 @@ const HANDLE_WIDTH = 8
 export const SelectionBox = memo(
   ({ onResizeHandlePointerDown }: SelectionBoxProps) => {
     const soloLayerId = useSelf((me) => me.presence.selection.length === 1 ? me.presence.selection[0] : null)
+
+    const isShowingHandles = useStorage((root) => soloLayerId && root.layers.get(soloLayerId)?.type !== LayerType.Path)
     return <div>SelectionBox</div>;
   }
 );
